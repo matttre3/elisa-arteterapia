448c2c18664296595d1e31b8a36dbc645270d5fd
matttre3 <3romano.matteo3@gmail.com>
2024-08-12 23:06:06 +0200

    fix: removing shadow from navbar and fixing z index on services circles

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import MobileNavbar from "./MobileNavbar";
 
 export default function Navbar() {
   return (
-    <div className="fixed left-0 top-0 z-50 flex w-full items-start justify-center bg-white shadow-md">
+    <div className="fixed left-0 top-0 z-50 flex w-full items-start justify-center bg-white">
       <div className="z-50 w-full max-w-screen-lg pb-3 pl-5 pr-5 pt-3 sm:pl-3 sm:pr-3">
         <Links mobile={false} />
         <MobileNavbar />
