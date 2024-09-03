bc2ec153d6f7baef41fb1624300cd80592d0e241
matttre3 <3romano.matteo3@gmail.com>
2024-09-03 11:57:16 +0200

    feat: adding embla carousel to the project

--- components/Servizi/Gallery.tsx
+++ components/Servizi/Gallery.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
-import { AnimatePresence, motion } from "framer-motion";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
 type GalleryProps = {
   images: string[];
@@ -9,131 +10,34 @@ type GalleryProps = {
 };
 
 export default function Gallery({ galleryType, images }: GalleryProps) {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()]);
 
-  const imagesPerSlide = galleryType === 1 ? 3 : 1;
-
-  const nextSlide = () => {
-    setCurrentIndex(
-      (prevIndex) => (prevIndex + imagesPerSlide) % images.length,
-    );
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex(
-      (prevIndex) =>
-        (prevIndex - imagesPerSlide + images.length) % images.length,
-    );
-  };
-
-  const openModal = (image: string) => {
-    setSelectedImage(image);
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setSelectedImage(null);
-    setIsModalOpen(false);
-  };
-
-  const currentImages = images.slice(
-    currentIndex,
-    currentIndex + imagesPerSlide,
-  );
+  useEffect(() => {
+    if (emblaApi) {
+      console.log(emblaApi.slideNodes());
+    }
+  }, [emblaApi]);
 
   return (
     <>
-      <div className="container relative w-full overflow-hidden pl-5 pr-5 md:pl-0 md:pr-0">
-        <div className="relative">
-          <AnimatePresence initial={false} mode="wait">
-            <motion.div
-              key={currentIndex}
-              className="flex justify-center gap-4"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.5 }}
-            >
-              {currentImages.map((image, index) => (
+      <div className="container">
+        <div className="overflow-hidden" ref={emblaRef}>
+          <div className="flex gap-10">
+            {images.map((image, index) => (
+              <div className="flex-[0_0_33%]">
                 <Image
                   key={index}
                   src={image}
-                  alt={`image-${currentIndex + index}`}
+                  alt="prova"
                   className="cursor-pointer object-cover"
                   width={600}
                   height={200}
-                  onClick={() => openModal(image)}
                 />
-              ))}
-            </motion.div>
-          </AnimatePresence>
-        </div>
-
-        <button
-          onClick={prevSlide}
-          className="absolute left-4 top-1/2 -translate-y-1/2 transform rounded-full bg-black bg-opacity-70 p-2 pb-4 text-4xl font-extrabold text-white"
-        >
-          &#8592;
-        </button>
-
-        <button
-          onClick={nextSlide}
-          className="absolute right-4 top-1/2 -translate-y-1/2 transform rounded-full bg-black bg-opacity-70 p-2 pb-4 text-4xl font-extrabold text-white"
-        >
-          &#8594;
-        </button>
-
-        <div className="mt-4 flex justify-center gap-2">
-          {Array.from({
-            length: Math.ceil(images.length / imagesPerSlide),
-          }).map((_, index) => (
-            <span
-              key={index}
-              className={`h-3 w-3 cursor-pointer rounded-full bg-black ${
-                currentIndex === index * imagesPerSlide
-                  ? "bg-opacity-100"
-                  : "bg-opacity-50"
-              }`}
-              onClick={() => setCurrentIndex(index * imagesPerSlide)}
-            ></span>
-          ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-
-      <AnimatePresence>
-        {isModalOpen && selectedImage && (
-          <motion.div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80 pl-5 pr-5"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            onClick={closeModal}
-          >
-            <motion.div
-              className="relative"
-              initial={{ scale: 0.8 }}
-              animate={{ scale: 1 }}
-              exit={{ scale: 0.8 }}
-            >
-              <Image
-                src={selectedImage}
-                alt="Full screen image"
-                width={800}
-                height={600}
-                className="rounded-lg shadow-lg"
-              />
-              <button
-                className="absolute right-2 top-2 text-2xl text-white"
-                onClick={closeModal}
-              >
-                &times;
-              </button>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
     </>
   );
 }
